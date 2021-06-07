5d051a04817a143906c3a774b79c77819ecfb29b
prateek <prateek.asthana@atscale.com>
2021-06-07 01:37:58 -0700

    able to filter only the files containing "Discover" requests

--- src/main/scala/com/prateek/xmlcompare/Main.scala
+++ src/main/scala/com/prateek/xmlcompare/Main.scala
@@ -1,22 +1,33 @@
 package com.prateek.xmlcompare
 
+import scala.xml.XML
+
+import java.io.File
+
 object Main extends App {
 
-  import scala.collection.mutable
-  import scala.xml.{Elem, Text, XML}
+  private def files(): Seq[File] = {
+    val dir = new File(getClass.getClassLoader.getResource("atscale-4").getPath)
+    dir.listFiles((d, name) => name.endsWith(".xml")).toList
+  }
 
-  val doc = XML.loadFile("src/main/resources/ssas-response.xml")
-  private val seq = doc \\ "row"
+  files().foreach(f => {
+    val doc = XML.loadFile(f)
+    val seq = doc \ "Body" \ "Discover"
+    println(seq.nonEmpty)
+    /*
   seq
     .foldLeft(new mutable.LinkedHashMap[String, String]())((a, b) => {
       val labelValuePairs = b.descendant.collect {
         case elem: Elem if elem.child.length == 1 =>
-          val value = (elem.child map {
-            case Text(data) => data
+          val value = (elem.child map { case Text(data) =>
+            data
           }).head
           (elem.label -> value)
       }
       a ++ labelValuePairs
-    }).foreach(println)
-//  println(seq)
+    })
+    .foreach(println)
+     */
+  })
 }
