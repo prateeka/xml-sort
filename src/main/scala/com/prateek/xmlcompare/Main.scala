490535dae2a7b918db1a652b18279566ad54595c
prateek <prateek.asthana@atscale.com>
2021-06-07 23:24:44 -0700

    add logback config and update code to use logger with appropriate logging level

--- src/main/scala/com/prateek/xmlcompare/Main.scala
+++ src/main/scala/com/prateek/xmlcompare/Main.scala
@@ -1,10 +1,11 @@
 package com.prateek.xmlcompare
-
 import scala.xml.Node
 
 import java.io.File
 
 object Main extends App {
+
+  val logger = com.typesafe.scalalogging.Logger(getClass)
 
   private val microDiscoverReqs = Discover(
     "atscale-micro"
@@ -13,14 +14,13 @@ object Main extends App {
     "atscale-small"
   )
 
-/*  microDiscoverReqs.foreach(x => {
-    println(x.file)
-    x.node.head.child.foreach(d => println(d))
-    println("----------------")
-  })*/
+  microDiscoverReqs.foreach(x => {
+    x.node.head.child.foreach(d => logger.debug(d.toString()))
+  })
 
-  private val crs: Seq[Comparator.ComparatorResult] = Comparator(microDiscoverReqs, smallDiscoverReqs)
-  crs.foreach(println)
+  private val crs =
+    Comparator(microDiscoverReqs, smallDiscoverReqs)
+  crs.foreach(cr=>logger.info(cr.toString))
 
 }
 case class XmlFile(file: File, node: Node)
