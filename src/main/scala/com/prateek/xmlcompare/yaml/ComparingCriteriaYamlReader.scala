9380f5694c2196c54c39e0e5aae0e64c820c4ba6
prateek <prateek.asthana@atscale.com>
2021-07-03 23:46:11 -0700

    working ComparingCriteriaYamlReader

--- src/main/scala/com/prateek/xmlcompare/yaml/ComparingCriteriaYamlReader.scala
+++ src/main/scala/com/prateek/xmlcompare/yaml/ComparingCriteriaYamlReader.scala
@@ -1,24 +1,26 @@
 package com.prateek.xmlcompare.yaml
 
+import scala.language.implicitConversions
+
 import scala.jdk.CollectionConverters.{ListHasAsScala, MapHasAsScala}
 
 import java.io.{File, FileInputStream}
 import java.util
 
-import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
+import org.yaml.snakeyaml.Yaml
 
-import com.prateek.xmlcompare.yaml.ComparingCriteriaYamlReader.{JList, JMap, StringSet}
+import com.prateek.xmlcompare.yaml.ComparingCriteriaYamlReader.{StringSet, StringToTMap}
+
 
 /** Reads [[ComparingCriteria]] from a yaml file.
   */
 object ComparingCriteriaYamlReader {
 
-  import scala.language.implicitConversions
-
   type JList = util.ArrayList[String]
-  type JMap[V] = util.Map[String, V]
+  type JMap[T] = util.Map[String, T]
   type StringSet = Set[String]
+  type StringToTMap[T] = Map[String, T]
 
   def apply(file: File): Seq[Criteria] = {
     val contents = {
@@ -27,11 +29,18 @@ object ComparingCriteriaYamlReader {
       val config: JMap[JMap[JList]] = yaml.load(input)
       config
     }
-    val criterias = contents.asScala.
-      map {
-        case ("request", v: JMap[JList])  => Criteria(v)
-        case ("response", v: JMap[JList]) => Criteria(v)
-      }
+    val sm: StringToTMap[StringToTMap[StringSet]] = {
+      val outerScalaMap = contents.asScala.toMap
+      outerScalaMap.map({ case (str, value) =>
+        val smjl = value.asScala.toMap
+        val smsl = smjl.map({ case (str, list) => str -> list.asScala.toSet })
+        str -> smsl
+      })
+    }
+    val criterias = sm.map {
+      case ("request", v)  => Criteria(v)
+      case ("response", v) => Criteria(v)
+    }
     criterias.foreach(println)
     criterias.toSeq
   }
@@ -43,20 +52,16 @@ case class Criteria(
   nodeConfig: Map[String, StringSet])
 
 object Criteria {
-  def apply(m: JMap[JList]): Criteria = {
-    val exclude: StringSet = toSet(m.asScala.get("exclude"))
-    val defaultInclude: StringSet = toSet(m.asScala.get("defaultInclude"))
-    val nodeConfig: Map[String, StringSet] = m.asScala.toMap.flatMap({
+  def apply(m: StringToTMap[StringSet]): Criteria = {
+    val exclude: StringSet = m.getOrElse("exclude", Set.empty)
+    val defaultInclude: StringSet = m.getOrElse("defaultInclude", Set.empty)
+    val nodeConfig: StringToTMap[StringSet] = m.flatMap({
       case ("exclude", _)        => Map.empty
       case ("defaultInclude", _) => Map.empty
-      case (k, v)                => Map(k -> toSet(Option(v)))
+      case (k, v)                => Map(k -> Option(v).getOrElse(Set.empty))
     })
     Criteria(exclude, defaultInclude, nodeConfig)
   }
 
-  private def toSet(m: Option[JList]): StringSet = {
-    m.map(_.asScala.toSet).
-      getOrElse(Set.empty)
-  }
 }
 
